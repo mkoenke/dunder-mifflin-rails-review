c09d83f62557b744f332f470e99104627331d9c5
mkoenke <mkoenke@sas.upenn.edu>
2020-10-30 16:00:58 -0400

    deliverables complete

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,30 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+    
+    def show
+        @dog = Dog.find(params[:id])
+    end
+    def new
+        @dog = Dog.new
+    end
+    def create
+        @dog = Dog.new(dog_params)
+        if @dog.save
+          redirect_to dog_path
+        else
+          
+        end
+    end
+
+  
+    
+    
+    private
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age, :employee_id)
+    end
+    
+    
 end
